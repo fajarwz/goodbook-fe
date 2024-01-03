3a48c8f02ee8d3f50df27f55f34b4734c7c93e61
Fajarwz <fajarwindhuzulfikar@gmail.com>
2024-01-03 20:30:04 +0700

    feat: published filter

--- src/member/features/browse/ContentFilterPublished.jsx
+++ src/member/features/browse/ContentFilterPublished.jsx
@@ -1,10 +1,40 @@
-import { Input } from "../../components/Form";
+import { useState } from 'react';
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 export default function ContentFilterPublished() {
+    const [startDate, setStartDate] = useState();
+    const [endDate, setEndDate] = useState();
+
+    const getLastDayOfMonth = (date) => {
+        return new Date(date.getFullYear(), date.getMonth() + 1, 0)
+    }
+
     return (
-        <div className="mb-4">
-            <Input placeholder='Published from...' name='published_from' addClassName='md:mb-2 md:mr-4 min-w-[185px]' />
-            <Input placeholder='Published until...' name='published_until' addClassName='min-w-[185px]' />
+        <div className="flex mb-4">
+            <DatePicker
+                placeholderText='Published from...'
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                selectsStart
+                startDate={startDate}
+                endDate={endDate}
+                dateFormat="MMMM yyyy"
+                showMonthYearPicker
+                className='h-11 border rounded-md border-gray-default p-3 bg-transparent min-w-[185px] md:mb-2 md:mr-4'
+            />
+            <DatePicker
+                placeholderText='Published until...'
+                selected={endDate}
+                onChange={(date) => setEndDate(getLastDayOfMonth(date))}
+                selectsEnd
+                startDate={startDate}
+                endDate={endDate}
+                minDate={startDate}
+                dateFormat="MMMM yyyy"
+                showMonthYearPicker
+                className='h-11 border rounded-md border-gray-default p-3 bg-transparent min-w-[185px]'
+            />
         </div>
     )
 }
