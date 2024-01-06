cf0f4af247181e9d6a25f7d2035a34dc00cdce44
Fajarwz <fajarwindhuzulfikar@gmail.com>
2024-01-06 14:29:34 +0700

    fix: log in label for admin

--- src/admin/features/login/Header.jsx
+++ src/admin/features/login/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div className="mb-10">
           <Logo />
-          <div className="text-center">Login to continue</div>
+          <div className="text-center">Log In to continue</div>
         </div>
     );
 }
