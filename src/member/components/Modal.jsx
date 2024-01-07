4ccef28e7c669dd391ea84c75bbfdd4777be76c6
Fajarwz <fajarwindhuzulfikar@gmail.com>
2024-01-07 15:34:50 +0700

    feat: review component and form

---
+++ src/member/components/Modal.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+export default function Modal({ children, onClose }) {
+  const dialog = useRef();
+
+  useEffect(() => {
+    const modal = dialog.current;
+    modal.showModal();
+
+    return () => {
+      modal.close();
+    };
+  }, []);
+
+  return createPortal(
+    <dialog ref={dialog} onClose={onClose} className="bg-white p-8 flex flex-col items-center justify-center rounded-xl fixed left-[calc(50%_-_20rem)] w-[40rem] max-h-[80vh] z-10 shadow-lg m-0 border-none top-[10vh] backdrop:h-screen backdrop:w-full backdrop:fixed backdrop:left-0 backdrop:top-0" >
+      {children}
+    </dialog>,
+    document.getElementById('modal')
+  );
+}
--- src/member/features/browse/detail/ContentDetailReviewsAuth.jsx
+++ src/member/features/browse/detail/ContentDetailReviewsAuth.jsx
@@ -1,10 +1,74 @@
-import { Rating } from "../../../components"
+import star1 from '../../../assets/img/star-1.svg'
+import starNotSet from '../../../assets/img/star-not-set.svg'
+import { useState } from "react"
+import Modal from '../../../components/Modal'
+import { Rating } from '../../../components'
+import { PrimaryButton, SecondaryButton } from '../../../components/Button'
 
 export default function ContentDetailReviewsAuth() {
+    const [starFill, setStarFill] = useState(Array(5).fill(false))
+    const [starChoosen, setStarChoosen] = useState(-1)
+    const [isSubmitting, setIsSubmitting] = useState(false)
+
+    const handleMouseHoverStar = (index) => {
+        setStarFill((prevStates) => prevStates.map((fill, i) => i <= index ? true : fill));
+    };
+
+    const handleMouseLeaveStar = (index) => {
+        setStarFill((prevStates) => prevStates.map((fill, i) => i === index && i > starChoosen ? false : fill));
+    };
+
+    const handleStarClicked = (index) => {
+        setStarChoosen(index);
+    };
+
+    const handleCancelReview = () => {
+        setStarChoosen(-1);
+    };
+
+    const handleSubmitReview = () => {
+        setIsSubmitting(true)
+
+
+
+        setIsSubmitting(false)
+    }
+
+    const generateStar = () => {
+        return [...Array(5)].map((_, index) => (
+            <img 
+                key={index} 
+                src={starFill[index] ? star1 : starNotSet} 
+                alt={`star-${index + 1}`} 
+                onMouseEnter={() => handleMouseHoverStar(index)} 
+                onMouseLeave={() => handleMouseLeaveStar(index)} 
+                onClick={() => handleStarClicked(index)} 
+                className='w-[1.625rem] h-[1.625rem]' 
+            />
+        ))
+    }
+
     return (
         <section className="mb-10 bg-white p-8 flex flex-col items-center justify-center rounded-xl">
             <h3 className="mb-4">What do you think?</h3>
-            <Rating rating={0} addClassName='items-center justify-center' addStarClassName='w-[1.625rem] h-[1.625rem]' />
+            <div className='flex gap-0.5'>
+                {generateStar()}
+            </div>
+            {starChoosen >= 0 && <Modal onClose={handleCancelReview}>
+                <h3 className="mb-4">What do you think?</h3>
+                <form onSubmit={handleSubmitReview} className='w-full'>
+                    <div className='mb-8'>
+                        <div className='flex items-center justify-center gap-0.5'>
+                            <Rating rating={starChoosen + 1} ratingLabel='' addStarClassName='w-[1.625rem] h-[1.625rem]' addClassName='mb-4' />
+                        </div>
+                        <textarea name='review' rows={3} className='w-full border rounded-md border-gray-default p-3 bg-transparent'></textarea>
+                    </div>
+                    <div className='text-center'>
+                        <PrimaryButton type='submit' addClassName='px-7 mr-2'>{isSubmitting ? 'Submitting...' : 'Submit Review'}</PrimaryButton>
+                        <SecondaryButton type='button' onClick={handleCancelReview}>Cancel</SecondaryButton>
+                    </div>
+                </form>
+            </Modal>}
         </section>
     )
 }
