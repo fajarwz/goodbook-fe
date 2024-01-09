95ee83e05178ab7d2df947c7f9a86f31b1e38e23
Fajarwz <fajarwindhuzulfikar@gmail.com>
2024-01-09 21:47:26 +0700

    chore: uninstall axios-retry, install react query devtools

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 
@@ -7,6 +8,7 @@ import router from './router';
 function App() {
   return <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    <ReactQueryDevtools />
   </QueryClientProvider>
 }
 
